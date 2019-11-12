cdf20aa2288108d4652722c539e9045343107926
emdavis7 <ericd@simpli.fi>
2019-11-12 16:33:17 -0600

    adds localstorage button uses the <script> function right now...

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+//$("#form").click(function(e) { e.preventDefault(); });
+
+function saveData(){
+  var heatInput = $("#heatLevel");
+  localStorage.setItem("heatLevel", heatInput.val());
+
+  var moistureInput = $("#moistureLevel");
+  localStorage.setItem("moistureLevel", moistureInput.val());
+  
+  var oceanicInput = $("#oceanicRate");
+  localStorage.setItem("oceanicRate", oceanicInput.val());
+  
+  var elevationInput = $("#elevationRate");
+  localStorage.setItem("elevationRate", elevationInput.val());
+  //console.log(localStorage);
+}
+
+$(document).ready( function() {
+    $('.btn btn-primary btn-lg').on('click', saveData());
+});
