5bc9a92c35546b67fa31158f303611dbef37ad6b
jamesEyoder <jeyoderjr@gmail.com>
2019-11-13 13:06:20 -0600

    Adding some blurbs

--- assets/js/paragraphs.js
+++ assets/js/paragraphs.js
@@ -1,6 +1,24 @@
 planet_names = ["Planet McPlanetface", "Earth 2", "Lathaetera", "Lasuanus", "Mengora", "Zolradus", "Dophus",
-"Vaphus", "Gulegantu", "Drivutis"," Llorth 4XCY", "Doth 9C7", "Hoth", "Mandalore", "Tatooine", "Ithor", 
+"Vaphus", "Gulegantu", "Drivutis"," Llorth 4XCY", "Doth 9C7", "Hoth", "Mandalore", "Tatooine", "Ithor",
 "Kepler-16b", "HD 40307g", "Kepler-186f", "PSO J318.5-22"]
+
 intro = "Welcome to {}! We hope you enjoy your stay. It looks like the temperature today is {}" +
 "and the moisture level is {}. There are many fun and exciting things to experience here on this exoplanet. " +
 "About () percent of our planet is covered in water, so I hope you packed your bathing suit! "
+
+blurb1="Howdy from {}! The oceans have turned to scorched blood and we couldn't be more excited." +
+"{} has everything: carbide fighting cages, chlorofluoro microtubes, and the last combination Pizza Hut-Taco Bell" +
+"in the Dreyfuss macrogalaxy. Book your flight today."
+
+blurb2="Comrade, salutations from {}! A smorgasbord of cabbage stew and kielbasa awaits" +
+" those travelling to the Zylinski zupragalaksi. Not a day goes by that doesn't see a salute to our" +
+" Dear Leader Boris Grutschev's eminent and august countenance which grants all life! Bow ye, before his all-giving eye."
+
+blurb3="Welcome to {}, the planet full of cryogenically preserved 20th century tennis stars!" +
+" See Sampras mid-serve, Borg barrelling to the net, McEnroe throwing one of his signature tantrums." +
+" The majestic Bille Jean King stands among the rest, racket raised, poised to strike."
+
+blurb4="Welcome to {}, the Kyralos Galaxy's special economic zone for grotesque taxonomic projects." +
+" No proof of provenance or intergalactic hunting license necessary! All carbon-based flesh mounts 50% off. "
+" Limited time special: Glantromphian Brakexsnout stuffed and hung for a single downpayment of 25M plus 1.25M per " +
+" selenial year until the end of the Visuncular cycle. Three for the price of two!" 
