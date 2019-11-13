eaf790ccf2512315f22bd7058c28e9f5b0d98b76
Brent Tucker <brent.tucker2014@gmail.com>
2019-11-13 10:26:34 -0600

    Updated planet names and intro

--- assets/js/paragraphs.js
+++ assets/js/paragraphs.js
@@ -1,3 +1,6 @@
-planet_names = ["Planet McPlanetface", "Earth 2"]
-fluff = ["Take a vacation here for so long you forget all of your passwords!",
- "This planet will have you saying 'Wow, this is a nice planet!'"]
+planet_names = ["Planet McPlanetface", "Earth 2", "Lathaetera", "Lasuanus", "Mengora", "Zolradus", "Dophus",
+"Vaphus", "Gulegantu", "Drivutis"," Llorth 4XCY", "Doth 9C7", "Hoth", "Mandalore", "Tatooine", "Ithor", 
+"Kepler-16b", "HD 40307g", "Kepler-186f", "PSO J318.5-22"]
+intro = "Welcome to {}! We hope you enjoy your stay. It looks like the temperature today is {}" +
+"and the moisture level is {}. There are many fun and exciting things to experience here on this exoplanet. " +
+"About () percent of our planet is covered in water, so I hope you packed your bathing suit! "
