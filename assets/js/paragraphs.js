cb22358e8bcb2b27ef32ef0adb53cdccc122c6d2
Brent Tucker <brent.tucker2014@gmail.com>
2019-11-14 08:05:39 -0600

    new blurb

--- assets/js/paragraphs.js
+++ assets/js/paragraphs.js
@@ -6,6 +6,8 @@ intro = "Welcome to {}! We hope you enjoy your stay. It looks like the temperatu
 "and the moisture level is {}. There are many fun and exciting things to experience here on this exoplanet. " +
 "About () percent of our planet is covered in water, so I hope you packed your bathing suit! "
 
+blurbs = [blurb1, blurb2, blurb3, blurb4, blurb4, blurb5]
+
 blurb1="Howdy from {}! The oceans have turned to scorched blood and we couldn't be more excited." +
 "{} has everything: carbide fighting cages, chlorofluoro microtubes, and the last combination Pizza Hut-Taco Bell" +
 "in the Dreyfuss macrogalaxy. Book your flight today."
@@ -21,4 +23,8 @@ blurb3="Welcome to {}, the planet full of cryogenically preserved 20th century t
 blurb4="Welcome to {}, the Kyralos Galaxy's special economic zone for grotesque taxonomic projects." +
 " No proof of provenance or intergalactic hunting license necessary! All carbon-based flesh mounts 50% off. "
 " Limited time special: Glantromphian Brakexsnout stuffed and hung for a single downpayment of 25M plus 1.25M per " +
-" selenial year until the end of the Visuncular cycle. Three for the price of two!" 
+" selenial year until the end of the Visuncular cycle. Three for the price of two!"
+
+blurb5="Welcome to {}! Known for it's sharp rock formations and acid lakes, we're certain your time here " +
+    "will be one for the books! While you're here, be sure to check out our new life-size amoeba petting zoo " +
+    "and visit our Cosmic Cafe for lunch."
